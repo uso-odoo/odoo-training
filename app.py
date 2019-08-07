e3b913a02aaf7e6a8e74dbfea69707fc30192a28
uso-odoo <uso@odoo.com>
2019-08-07 10:07:26 +0530

    Bind on click event of Search Button

--- app.py
+++ app.py
@@ -9,46 +9,41 @@ from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
 import jinja2
 import csv
+import json
 
-
-class Shortly(object):
+class Shop(object):
 
     def __init__(self):
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
-                                     autoescape=True)
+                                     autoescape=True)  
         self.url_map = Map([
             Rule('/', endpoint='index_url'),
             Rule('/search', endpoint='search_data')
         ])
     
     def index_url(self, request):
-        #print("test", dir(request))
         #import pdb;pdb.set_trace()
-        data = self.csvreader()
+        data = self.csv_reader()
         return self.render_template('index.html',data=data)
 
-    def search_data(self):
-    
-        fin = open('Products/data.csv')
+    def search_data(self, request):
+        found = []
         if request.method == "POST":
-            word = request.param.get('search_string')
-            found = {}
-            for line in fin:
-                #str1,id=line.split(',')
-                #for w in words:
-                if words in line:
-                        #print(line)
-                    found[words]=line          
-        return found
+            word = request.args.get('search_string')
+            with open('Products/data.csv', 'r') as f_in:
+                csv_reader = csv.reader(f_in, delimiter=',', quotechar='"')
+                lines = [line for line in csv_reader if line]
+                data = [dict(zip(lines[0], l)) for l in lines[1:]]
 
-    def csvreader(self,ser=None):
+            found = [i for i in data if word in i['product'].strip()]
 
+        return Response(json.dumps(found), mimetype='application/json')     
+
+    def csv_reader(self):
         rdr= csv.reader( open("Products/data.csv", "r" ) )
         header = next(rdr,None)
         csv_data = [ row for row in rdr ]
-
-
         return csv_data
 
     def dispatch_request(self, request):
@@ -62,19 +57,20 @@ class Shortly(object):
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
         response = self.dispatch_request(request)
-        return response(environ, start_response)
+        return response(environ, start_response) 
 
     def render_template(self, template_name, **context):
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
 
     def __call__(self, environ, start_response):
+        
         return self.wsgi_app(environ, start_response)
 
 def create_app():
-    app = Shortly()
-    return app
+    app = Shop()
 
+    return app
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
