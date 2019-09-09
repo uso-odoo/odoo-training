4d4dc1a4f57fe0650575b9354bc71aa857c0703c
uso-odoo <uso@odoo.com>
2019-09-09 14:42:05 +0530

    Widget Created

---
+++ Week-4 (JavaScript)/openacademy/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from. import models
+
+
+# access_openacademy_course,access.openacademy.course,model_openacademy_course,base.group_user,1,1,1,1
+# access_openacademy_session,access.openacademy.session,model_openacademy_session,base.group_user,1,1,1,1
+
+
+# access_manager,openacademy.course.manager,model_openacademy_course,openacademy.group_openacademy_manager,1,1,1,1
+# access_user,openacademy.course.manager,model_openacademy_course,openacademy.group_openacademy_user,1,1,0,0
+# access_manager_s,openacademy.course.manager,model_openacademy_session,openacademy.group_openacademy_manager,1,1,1,1
+# access_user_s,openacademy.course.manager,model_openacademy_session,openacademy.group_openacademy_user,1,1,0,0
+# access_academy_teachers,access_academy_teachers,model_academy_teachers,,1,1,1,0
+# access_academy_courses,access_academy_courses,model_academy_courses,,1,1,1,0
